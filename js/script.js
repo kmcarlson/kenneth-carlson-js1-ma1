5f5447e395edde0d96c698a4262decd1ffa32d54
kmcarlson <77496513+kmcarlson@users.noreply.github.com>
2021-10-25 09:22:37 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+// Question 1
+
+const cat = {
+    name: "Henrik",
+    age: 27,
+    complain: function() {
+        console.log("Meow!")
+    }
+}
